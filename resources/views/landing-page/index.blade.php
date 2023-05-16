7460b8476419b83ee4ecf9a6a626fc1457b11c2f
naufalMHS <naupalf04@mhs.itenas.ac.id>
2023-05-16 12:23:35 +0700

    final project

--- resources/views/landing-page/index.blade.php
+++ resources/views/landing-page/index.blade.php
@@ -13,10 +13,7 @@
         <div class="hidden lg:block lg:absolute lg:z-10 lg:top-0 lg:right-0" aria-hidden="true">
             <img src="{{ asset('/assets/frontsite/images/hero-image.png') }}"
                 class="bg-cover bg-center object-cover object-center max-h-[580px]" alt="Hero Image" />
-            <div
-                class="text-center absolute bottom-0 -left-20 -translate-y-14 bg-white px-7 py-5 rounded-xl shadow-2xl">
-
-            </div>
+        </div>
         </div>
 
         <!-- Hero Description -->
@@ -27,16 +24,12 @@
                     <!-- Label New -->
                     <h1>
                         <div class="flex items-center">
-                            <span
-                                class="text-white text-xs sm:text-sm font-medium bg-[#2AB49B] rounded-full px-8 py-2">New</span>
-                            <span
-                                class="text-[#1E2B4F] text-[11px] sm:text-sm bg-[#F2F6FE] rounded-r-full px-8 py-2 relative -z-10 -ml-4">Emergency
-                                call feature updated</span>
+
                         </div>
 
                         <span class="mt-6 block text-4xl font-semibold sm:text-5xl">
-                            <span class="block text-[#1E2B4F] leading-normal">Meet Your Doctor. <br />Trusted &
-                                Professional.</span>
+                            <span class="block text-[#1E2B4F] leading-normal">Selamat Datang <br />Family Dental
+                                Care</span>
                         </span>
                     </h1>
                     <!-- End Label New -->
@@ -44,22 +37,10 @@
                     <!-- Text -->
                     <div class="flex flex-wrap gap-16 mt-8">
                         <div class="flex items-center gap-4">
-                            <div class="flex-shrink-0">
-                                <img src="{{ asset('/assets/frontsite/images/service.svg') }}" alt="service icon" />
-                            </div>
-                            <div>
-                                <h5 class="text-[#1E2B4F] text-lg font-medium">Best Recipe</h5>
-                                <p class="text-[#AFAEC3]">for your medicine</p>
-                            </div>
+
                         </div>
                         <div class="flex items-center gap-4">
-                            <div class="flex-shrink-0">
-                                <img src="{{ asset('/assets/frontsite/images/service.svg') }}" alt="service icon" />
-                            </div>
-                            <div>
-                                <h5 class="text-[#1E2B4F] text-lg font-medium">Free Consultation</h5>
-                                <p class="text-[#AFAEC3]">as we promised</p>
-                            </div>
+
                         </div>
                     </div>
                     <!-- Text -->
@@ -104,33 +85,41 @@
         </div>
     </section>
     <section id="doctor" class="mt-4 lg:mt-16">
-        <div class="section-title">
-            <h3 class="text-[#1E2B4F] text-2xl font-semibold">Dokter Terbaik</h3>
-            <p class="text-[#A7B0B5] mt-2">Membantu kehidupan Anda menjadi lebih baik</p>
 
-            <div class="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-12 lg:gap-10 mt-10">
-                @foreach ($dokter as $data)
-                <div class="relative">
-                    <div class="w-full h-[350px] rounded-2xl overflow-hidden">
-                        <a href="{{ Auth::check() ? '/appointment/'.Crypt::encrypt($data->id_dokter) : '/login' }}"
-                            class="group">
-                            <img src="{{ asset('img/dokter/'.$data->images) }}"
-                                class="w-full h-full bg-center bg-no-repeat object-cover object-center"
-                                alt="{{ $doctor_item->name ?? '' }}">
-                            <div
-                                class="opacity-0 group-hover:opacity-100 transition-all ease-in absolute inset-0 bg-[#0D63F3] bg-opacity-70 flex justify-center items-center">
-                                <span class="text-[#0D63F3] font-medium bg-white rounded-full px-8 py-3">Book Now</span>
-                            </div>
-                        </a>
+        <div class="mx-auto max-w-7xl px-4 lg:px-14 py-14">
+            <div class="section-title">
+                <h3 class="text-[#1E2B4F] text-2xl font-semibold">List Dokter</h3>
+                <p class="text-[#A7B0B5] mt-2">Membantu kehidupan Anda menjadi lebih baik</p>
+
+                <div class="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-12 lg:gap-10 mt-10">
+                    @foreach ($dokter as $data)
+                    <div class="relative">
+                        <div class="w-full h-[350px] rounded-2xl overflow-hidden">
+                            <a href="{{ Auth::check() ? '/appointment/'.Crypt::encrypt($data->id_dokter) : '/login' }}"
+                                class="group">
+                                <img src="{{ asset('img/dokter/'.$data->images) }}"
+                                    class="w-full h-full bg-center bg-no-repeat object-cover object-center"
+                                    alt="{{ $doctor_item->name ?? '' }}">
+                                <div
+                                    class="opacity-0 group-hover:opacity-100 transition-all ease-in absolute inset-0 bg-[#0D63F3] bg-opacity-70 flex justify-center items-center">
+                                    <span class="text-[#0D63F3] font-medium bg-white rounded-full px-8 py-3">Book
+                                        Now</span>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="bg-[#0D63F3] px-4 py-2">
+                            <h4 class="text-white text-sm mb-2">{{ $data->nama_dokter }}</h4>
+
+                        </div>
                     </div>
-                    <div class="bg-[#0D63F3] px-4 py-2">
-                        <h4 class="text-white text-sm mb-2">{{ $data->nama_dokter }}</h4>
-                        <h4 class="text-white text-sm">Jadwal: {{ $data->jadwal_dokter }}</h4>
-                    </div>
+
+                    @endforeach
                 </div>
-                @endforeach
             </div>
         </div>
+        </div>
+
+
     </section>
 
     <!-- ======= Gallery Section ======= -->
